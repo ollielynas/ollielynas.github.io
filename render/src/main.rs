a2deeab6a1611cc18375fe211eccd76ab13dfef5
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-05-30 22:18:14 +1200

    fixed mobile z-index issue

--- render/src/main.rs
+++ render/src/main.rs
@@ -1,9 +1,17 @@
+
 use macroquad::{
     experimental::state_machine::State,
     miniquad::{window::screen_size, EventHandler},
     prelude::*,
 };
 use site::{SiteEvent, SiteState};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache::new(capacity);
+ * let ret_1: i32 = obj.get(key);
+ * obj.put(key, value);
+ */
 
 mod boids;
 mod home;
--- render/src/site.rs
+++ render/src/site.rs
@@ -56,3 +56,6 @@ impl SiteState {
 
 
 
+
+
+
