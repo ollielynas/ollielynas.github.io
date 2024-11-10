54929243c90b1f1dea40813581dbd0b89dfc4038
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2024-11-10 17:55:55 +1300

    updated 404

---
+++ 404.js
@@ -0,0 +1,13 @@
+fetch("https://api.github.com/users/ollielynas/repos")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
